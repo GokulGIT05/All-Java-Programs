9574952b3c7d87ff2c2bb1cad770306793784d76
Gokul - Tortoise <gkgopig02@gmail.com>
2020-01-06 23:09:13 +0530

    All My Programs - Integrated One

---
+++ src/com/gk1/jbPrograms/Gk1FizzBuzz.java
@@ -0,0 +1,37 @@
+package com.gk1.jbPrograms;
+
+import java.util.Scanner;
+
+public class Gk1FizzBuzz {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter the Value: "
+				+ "");
+		int value = scan.nextInt();
+
+		printFizzBuzz(value);
+
+		scan.close();
+	}
+
+	// Problem Statement: Multiples of 3 Print Fizz, Multiples of 5 Print Buzz,
+	// Multiples of Both 3 & 5 Print FIZZBUZZ
+	private static void printFizzBuzz(int value) {
+
+		for (int i = 1; i <= value; i++) {
+			if (i % 3 == 0 && i % 5 == 0) {
+				System.out.println("FIZZBUZZ");
+			} else if (i % 5 == 0) {
+				System.out.println("BUZZ");
+			} else if (i % 3 == 0) {
+				System.out.println("FIZZ");
+			} else {
+				System.out.println(i);
+			}
+		}
+
+	}
+
+}
